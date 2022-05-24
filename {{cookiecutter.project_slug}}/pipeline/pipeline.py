fabf573c216f48386baab2c4c987a4b7859c1190
João André Da Silva Cunha <joao.silva1@avast.com>
2022-05-24 09:51:32 +0200

    Blank wanna cookie cutter template

---
+++ {{cookiecutter.project_slug}}/pipeline/pipeline.py
@@ -0,0 +1,41 @@
+# ignore: import-error
+# pylint: disable = no-value-for-parameter
+from pathlib import Path
+
+from google_cloud_pipeline_components import aiplatform as aip_components
+from kfp.v2 import dsl
+from kfp.v2.dsl import component
+
+import config as cfg
+
+
+@component(
+    base_image="python:3.9",
+)
+def on_exit():
+    import logging
+
+    logging.getLogger().setLevel(logging.INFO)
+
+    logging.info("This Component will run on exit, as last")
+
+
+@dsl.pipeline(
+    # A name for the pipeline. Use to determine the pipeline Context.
+    name=cfg.PIPELINE_NAME,
+    pipeline_root=cfg.PIPELINE_ROOT
+)
+def wanna_pipeline(eval_acc_threshold: float):
+    pipeline_dir = Path(__file__).parent.resolve()
+
+    # ===================================================================
+    # Get pipeline result notification
+    # ===================================================================
+    exit_task = (
+        on_exit()
+        .set_display_name("On Exit Dummy Task")
+        .set_caching_options(False)
+    )
+
+    with dsl.ExitHandler(exit_task):
+        pass
